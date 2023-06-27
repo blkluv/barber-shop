5d5c2cb0ad080180f6ceb70302503a4ae91f3bd0
mariushor <marius.horghidan@yahoo.com>
2023-06-28 00:21:51 +0300

    updated the implementation of the google maps instance

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -13,7 +13,7 @@ export const Map = () => {
         loading="lazy"
         allowFullScreen
         className="h-screen w-full py-6 sm:h-140"
-        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJsSF9mMb7ykARYnheKk3oCxo&key=AIzaSyD0Zp-jrwkShUgbFsTyxe6yG5j33tIuA7k"
+        src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJsSF9mMb7ykARYnheKk3oCxo&key=${process.env.NEXT_PUBLIC_API_KEY}`}
       />
     </section>
   );
